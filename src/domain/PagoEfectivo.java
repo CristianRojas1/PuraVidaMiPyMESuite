4d0e10c06496f458dec810473b43465680600a0d
User <User@CristianRojas>
2025-08-19 14:57:17 -0600

    git commit 5 " Se creo las clases Pago y la PagoEfectivo "

---
+++ src/domain/PagoEfectivo.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+public class PagoEfectivo implements Pago {
+    public boolean procesar(double monto) { return true; }
+    public String tipo() { return "Efectivo"; }
+}

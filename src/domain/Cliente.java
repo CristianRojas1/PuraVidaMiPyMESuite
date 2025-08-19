2ecbedf381c0911c5c8e562121338497ad221644
User <User@CristianRojas>
2025-08-19 14:50:27 -0600

    git commit 4 " Se creo las clases Cliente y la Orden "

--- src/domain/Cliente.java
+++ src/domain/Cliente.java
@@ -4,10 +4,18 @@
  */
 package domain;
 
-/**
- *
- * @author User
- */
 public class Cliente {
-    
+    public String idInterno;
+    public String nombre;
+    public String correo;
+    public String telefono;
+    public String cedulaCifrada;
+
+    public Cliente(String idInterno, String nombre, String correo, String telefono, String cedulaCifrada) {
+        this.idInterno = idInterno;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.cedulaCifrada = cedulaCifrada;
+    }
 }
--- src/domain/Orden.java
+++ src/domain/Orden.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package domain;
+import java.util.List;
 
-/**
- *
- * @author User
- */
 public class Orden {
-    
+    public String id;
+    public Cliente cliente;
+    public List<ItemOrden> items;
+    public double totalBruto;
+    public double impuestos;
+    public double descuentos;
+    public double totalNeto;
+    public String fecha;
+
+    public Orden(String id, Cliente cliente, List<ItemOrden> items, String fecha) {
+        this.id = id;
+        this.cliente = cliente;
+        this.items = items;
+        this.fecha = fecha;
+    }
 }
